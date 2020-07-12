0cfa0c2dbc33b67f5a0f115abd2036515ff6506c
Haydrent <handersen_isaac@yahoo.com>
2020-07-12 22:05:10 +0700

    Updated webkitautofill for loginv2

--- Web App/client/src/components/misc/global-styles/GlobalStyles.js
+++ Web App/client/src/components/misc/global-styles/GlobalStyles.js
@@ -67,6 +67,7 @@ export const globalStyles = responsiveFontSizes(createMuiTheme({
           borderBottom: "none",
           boxShadow: "none",
           margin: "0px 15px 0px 15px",
+          WebkitBoxShadow: "0 0 0 1000px white inset",
         },
       },
     },
