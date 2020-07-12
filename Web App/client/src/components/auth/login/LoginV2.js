c6ed3d1bf10b3b109325395688fd592daaf4279d
Haydrent <handersen_isaac@yahoo.com>
2020-07-12 22:55:33 +0700

    Updated loginv2 email not found error still wont show up

--- Web App/client/src/components/auth/login/LoginV2.js
+++ Web App/client/src/components/auth/login/LoginV2.js
@@ -5,25 +5,36 @@ import PropTypes from "prop-types";
 import classnames from "classnames";
 import { loginUser } from "../../../actions/UserActions";
 import authBackground from "../AuthBackground.png";
-import { Button, Divider, FormControl, Grid, IconButton, InputAdornment, Link, MenuItem, Paper, OutlinedInput, Select, TextField, Typography } from "@material-ui/core";
+import { Button, Divider, Grid, Hidden, IconButton, InputAdornment, Link, Paper, TextField, Typography } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 import ErrorIcon from "@material-ui/icons/Error";
 import VisibilityIcon from "@material-ui/icons/Visibility";
 import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
 
 const styles = (theme) => ({
-  root: {
+  rootDesktop: {
     margin: "auto",
     maxWidth: "1000px",
     height: "500px",
+    padding: "10px",
     backgroundImage: `url(${authBackground})`,
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
     backgroundSize: "contain",
   },
+  rootMobile: {
+    margin: "auto",
+    maxWidth: "1000px",
+    height: "500px",
+    padding: "10px",
+    backgroundImage: `url(${authBackground})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  },
   container: {
     margin: "auto",
-    maxWidth: "400px",
+    maxWidth: "350px",
     padding: "40px",
   },
   errorInfo: {
@@ -95,102 +106,209 @@ class LoginV2 extends Component {
     document.body.style = "background: linear-gradient(#6A8CF6, #FFFFFF); background-repeat: no-repeat";
 
     return(
-      <div className={classes.root}>
-        <Paper className={classes.container}>
-          <Grid container direction="column" spacing={6}>
-            <Grid item>
-              <Typography variant="h6" align="center">
-                <b>Masuk ke Schooly</b>
-              </Typography>
-            </Grid>
-            <Grid item>
-              <form noValidate onSubmit={this.onSubmit}>
-                <Grid container direction="column" spacing={4} alignItems="stretch">
-                  <Grid item>
-                    <TextField
-                      fullWidth
-                      variant="outlined"
-                      id="email"
-                      onChange={this.onChange}
-                      value={this.state.email}
-                      error={errors.email}
-                      type="email"
-                      helperText={
-                        <div style={{ display:"flex", alignItems: "center"}}>
-                          {errors.email || errors.emailnotfound ? <ErrorIcon style={{ height: "5%", width:"5%"}} /> : null}
-                          <Typography variant="h8" style={{marginLeft: "4px"}}>
-                            {errors.email}
-                            {errors.emailnotfound}
-                          </Typography>
-                        </div>
-                      }
-                      className={classnames("", {
-                        invalid: errors.email || errors.emailnotfound
-                      })}
-                    />
-                  </Grid>
-                  <Grid item container>
-                    <TextField
-                      fullWidth
-                      variant="outlined"
-                      id="password"
-                      onChange={this.onChange}
-                      value={this.state.password}
-                      type={passwordIsMasked ? "password" : "text"}
-                      error={errors.password}
-                      helperText={
-                        <div style={{ display:"flex", alignItems: "center"}}>
-                          {errors.password || errors.passwordincorrect ? <ErrorIcon style={{ height: "5%", width:"5%"}} /> : null}
-                          <Typography variant="h8" style={{marginLeft: "4px"}}>
-                            {errors.password}
-                            {errors.passwordincorrect}
-                          </Typography>
-                        </div>
-                      }
-                      className={classnames("", {
-                        invalid: errors.password || errors.passwordincorrect
-                      })}
-                      InputProps={{
-                        endAdornment:
-                          <InputAdornment position="end">
-                            <IconButton
-                              size="small"
-                              onClick={this.togglePasswordVisibility}
-                            >
-                              {icon ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                            </IconButton>
-                          </InputAdornment>,
-                      }}
-                    />
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      variant="contained"
-                      type="submit"
-                      style={{
-                        backgroundColor: "#61BD4F",
-                        color: "white",
-                        width: "100%",
-                      }}
-                    >
-                      Masuk
-                    </Button>
-                  </Grid>
+      <div>
+        <Hidden smUp implementation="css">
+          <div className={classes.rootMobile}>
+            <Paper className={classes.container}>
+              <Grid container direction="column" spacing={5}>
+                <Grid item>
+                  <Typography variant="h6" align="center">
+                    <b>Masuk ke Schooly</b>
+                  </Typography>
                 </Grid>
-              </form>
-            </Grid>
-            <Divider />
-            <Grid item container justify="space-around">
-              <Link href="/akun/lupa-katasandi">
-                Lupa Kata Sandi?
-              </Link>
-              |
-              <Link href="/daftar">
-                Belum ada Akun?
-              </Link>
-            </Grid>
-          </Grid>
-        </Paper>
+                <Grid item>
+                  <form noValidate onSubmit={this.onSubmit}>
+                    <Grid container direction="column" spacing={4} alignItems="stretch">
+                      <Grid item>
+                        <label>Email</label>
+                        <TextField
+                          fullWidth
+                          variant="outlined"
+                          id="email"
+                          onChange={this.onChange}
+                          value={this.state.email}
+                          error={errors.email}
+                          type="email"
+                          helperText={
+                            <div style={{ display:"flex", alignItems: "center"}}>
+                              {errors.email || errors.emailnotfound ? <ErrorIcon style={{ height: "5%", width:"5%"}} /> : null}
+                              <Typography variant="h8" style={{marginLeft: "4px"}}>
+                                {errors.email}
+                                {errors.emailnotfound}
+                              </Typography>
+                            </div>
+                          }
+                          className={classnames("", {
+                            invalid: errors.email || errors.emailnotfound
+                          })}
+                        />
+                      </Grid>
+                      <Grid item>
+                        <label>Kata Sandi</label>
+                        <TextField
+                          fullWidth
+                          variant="outlined"
+                          id="password"
+                          onChange={this.onChange}
+                          value={this.state.password}
+                          type={passwordIsMasked ? "password" : "text"}
+                          error={errors.password}
+                          helperText={
+                            <div style={{ display:"flex", alignItems: "center"}}>
+                              {errors.password || errors.passwordincorrect ? <ErrorIcon style={{ height: "5%", width:"5%"}} /> : null}
+                              <Typography variant="h8" style={{marginLeft: "4px"}}>
+                                {errors.password}
+                                {errors.passwordincorrect}
+                              </Typography>
+                            </div>
+                          }
+                          className={classnames("", {
+                            invalid: errors.password || errors.passwordincorrect
+                          })}
+                          InputProps={{
+                            endAdornment:
+                              <InputAdornment position="end">
+                                <IconButton
+                                  size="small"
+                                  onClick={this.togglePasswordVisibility}
+                                >
+                                  {icon ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                                </IconButton>
+                              </InputAdornment>,
+                          }}
+                        />
+                      </Grid>
+                      <Grid item>
+                        <Button
+                          variant="contained"
+                          type="submit"
+                          style={{
+                            backgroundColor: "#61BD4F",
+                            color: "white",
+                            width: "100%",
+                          }}
+                        >
+                          Masuk
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </form>
+                </Grid>
+                <Divider />
+                <Grid item container justify="space-around">
+                  <Link href="/akun/lupa-katasandi">
+                    Lupa Kata Sandi?
+                  </Link>
+                  |
+                  <Link href="/daftar">
+                    Belum ada Akun?
+                  </Link>
+                </Grid>
+              </Grid>
+            </Paper>
+          </div>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <div className={classes.rootDesktop}>
+            <Paper className={classes.container}>
+              <Grid container direction="column" spacing={5}>
+                <Grid item>
+                  <Typography variant="h6" align="center">
+                    <b>Masuk ke Schooly</b>
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <form noValidate onSubmit={this.onSubmit}>
+                    <Grid container direction="column" spacing={4} alignItems="stretch">
+                      <Grid item>
+                        <label>Email</label>
+                        <TextField
+                          fullWidth
+                          variant="outlined"
+                          id="email"
+                          onChange={this.onChange}
+                          value={this.state.email}
+                          error={errors.email}
+                          type="email"
+                          helperText={
+                            <div style={{ display:"flex", alignItems: "center"}}>
+                              {errors.email || errors.emailnotfound ? <ErrorIcon style={{ height: "5%", width:"5%"}} /> : null}
+                              <Typography variant="h8" style={{marginLeft: "4px"}}>
+                                {errors.email}
+                                {errors.emailnotfound}
+                              </Typography>
+                            </div>
+                          }
+                          className={classnames("", {
+                            invalid: errors.email || errors.emailnotfound
+                          })}
+                        />
+                      </Grid>
+                      <Grid item>
+                        <label>Kata Sandi</label>
+                        <TextField
+                          fullWidth
+                          variant="outlined"
+                          id="password"
+                          onChange={this.onChange}
+                          value={this.state.password}
+                          type={passwordIsMasked ? "password" : "text"}
+                          error={errors.password}
+                          helperText={
+                            <div style={{ display:"flex", alignItems: "center"}}>
+                              {errors.password || errors.passwordincorrect ? <ErrorIcon style={{ height: "5%", width:"5%"}} /> : null}
+                              <Typography variant="h8" style={{marginLeft: "4px"}}>
+                                {errors.password}
+                                {errors.passwordincorrect}
+                              </Typography>
+                            </div>
+                          }
+                          className={classnames("", {
+                            invalid: errors.password || errors.passwordincorrect
+                          })}
+                          InputProps={{
+                            endAdornment:
+                              <InputAdornment position="end">
+                                <IconButton
+                                  size="small"
+                                  onClick={this.togglePasswordVisibility}
+                                >
+                                  {icon ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                                </IconButton>
+                              </InputAdornment>,
+                          }}
+                        />
+                      </Grid>
+                      <Grid item>
+                        <Button
+                          variant="contained"
+                          type="submit"
+                          style={{
+                            backgroundColor: "#61BD4F",
+                            color: "white",
+                            width: "100%",
+                          }}
+                        >
+                          Masuk
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </form>
+                </Grid>
+                <Divider />
+                <Grid item container justify="space-around">
+                  <Link href="/akun/lupa-katasandi">
+                    Lupa Kata Sandi?
+                  </Link>
+                  |
+                  <Link href="/daftar">
+                    Belum ada Akun?
+                  </Link>
+                </Grid>
+              </Grid>
+            </Paper>
+          </div>
+        </Hidden>
       </div>
     )
   }
