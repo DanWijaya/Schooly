19e5d98ac9513d24eb7f21c273e8713d573a0b77
DanWijaya <danwijaya19@gmail.com>
2020-07-30 14:36:14 +0800

    Implemented icons on Dropbox page

--- Web App/client/src/components/dropbox/DropboxConnect.js
+++ Web App/client/src/components/dropbox/DropboxConnect.js
@@ -133,15 +133,17 @@ const handleUpdatePath = useCallback((path) => {
 
 const getLinkToFile = useCallback((path) => {
   // console.log(path);
-  let dropbox = new Dropbox({ accessToken: dropbox_token });
+  let dropbox = new Dropbox({ fetch: fetch, accessToken: dropbox_token });
+  
   dropbox
-    .filesGetTemporaryLink({ path: path })
+    .filesGetTemporaryLink({ path: path})
     .then((response) => {
-      window.location.href = response.link;
+      window.location.href = response.link
     })
     .catch((error) => {
       console.error(error, 'Error by downloading file');
-    });
+    })
+
 },[dropbox_token]);
 
 const ViewDirectory = (folders) => {
