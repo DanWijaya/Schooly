fb9724b83267eb814e3b09ef56beb683140c63f0
Haydrent <handersen_isaac@yahoo.com>
2020-08-03 15:55:03 +0700

    Attempt to try CountdownCicrcleTimer on viewtaskstudent

--- Web App/client/src/components/objects/assessment/ViewAssessmentStudent.js
+++ Web App/client/src/components/objects/assessment/ViewAssessmentStudent.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import LightTooltip from "../../misc/light-tooltip/LightTooltip";
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 import { Button, Divider, FormControl, FormControlLabel, Grid, IconButton, Paper, Radio, RadioGroup, TablePagination, TextField, Typography } from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import AddIcon from "@material-ui/icons/Add";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
@@ -10,226 +11,109 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import FilterNoneIcon from "@material-ui/icons/FilterNone";
 import SaveIcon from "@material-ui/icons/Save";
 
-const styles = (theme) => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     margin: "auto",
     maxWidth: "1000px",
     padding: "10px",
   },
-  paperContent: {
+  content: {
     padding: "20px",
   },
-  pageButton: {
-    width: "35px",
-    height: "35px",
-    padding: "0px",
-    backgroundColor: theme.palette.action.selected,
-    color: "grey",
-    "&:focus, &:hover": {
-      backgroundColor: theme.palette.divider,
-      color: "grey",
-    },
-  },
-  addQuestionButton: {
-    width: "35px",
-    height: "35px",
-    padding: "0px",
-    backgroundColor: theme.palette.primary.main,
-    color: "white",
-    "&:focus, &:hover": {
-      backgroundColor: "white",
-      color: theme.palette.primary.main,
-    },
-  },
-  draftAssessmentButton: {
-    width: "35px",
-    height: "35px",
-    padding: "0px",
-    backgroundColor: theme.palette.warning.main,
-    color: "white",
-    "&:focus, &:hover": {
-      backgroundColor: "white",
-      color: theme.palette.warning.main,
-    },
-  },
-  createAssessmentButton: {
-    backgroundColor: theme.palette.create.main,
-    color: "white",
-    "&:focus, &:hover": {
-      backgroundColor: "white",
-      color: theme.palette.create.main,
-    },
-  },
-});
+}));
 
-class ViewAssessmentStudent extends Component {
-  constructor() {
-    super();
-    this.state = {
-      radioValue: "option1",
-    }
-  }
-
-  handleRadioValue = (event) => {
-    this.setState(event.target.value);
-  };
-
-  render() {
-    const { classes } = this.props;
-
-    document.title = "Schooly | Buat Kuis";
-
-    return (
-      <div className={classes.root}>
-        <form>
-          <Grid container direction="column" spacing={3}>
-            <Grid item>
-              <Paper>
-                <div className={classes.paperContent}>
-                  <Typography variant="h5" gutterBottom>
-                    <b>Buat Kuis</b>
-                  </Typography>
-                  <Typography color="textSecondary">
-                    Tambahkan keterangan kuis untuk membuat kuis.
-                  </Typography>
-                </div>
-                <Divider />
-                <Grid container direction="column" spacing={3} justify="center" className={classes.paperContent}>
-                  <Grid item>
-                    <Typography component="label" for="name" color="primary">
-                      Judul
-                    </Typography>
-                    <TextField
-                      fullWidth
-                      variant="outlined"
-                      id="name"
-                    />
-                  </Grid>
-                  <Grid item>
-                    <Typography component="label" for="description" color="primary">
-                      Deskripsi
-                    </Typography>
-                    <TextField
-                      multiline
-                      fullWidth
-                      variant="outlined"
-                      id="description"
-                    />
-                  </Grid>
-                </Grid>
-              </Paper>
-            </Grid>
-            <Grid item>
-              <Paper>
-                <Grid container>
-                  <Grid item xs sm className={classes.paperContent}>
-                    <Grid container direction="column" spacing={2}>
-                      <Grid item>
-                        <Typography variant="h6" gutterBottom>
-                          Soal 1
-                        </Typography>
-                        <TextField multiline fullWidth variant="filled" />
-                      </Grid>
-                      <Grid item>
-                        <FormControl component="fieldset">
-                          <RadioGroup value={this.state.radioValue} onChange={this.handleRadioValue}>
-                            <FormControlLabel value="female" control={<Radio />} label="Female" />
-                            <FormControlLabel value="male" control={<Radio />} label="Male" />
-                            <FormControlLabel value="other" control={<Radio />} label="Other" />
-                            <FormControlLabel value="disabled" disabled control={<Radio />} label="(Disabled option)" />
-                            <FormControlLabel disabled control={<Radio />} label={<TextField />} />
-                          </RadioGroup>
-                        </FormControl>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                  <Divider flexItem orientation="vertical" />
-                  <Grid item xs={3} sm={1} className={classes.paperContent}>
-                    <Grid container direction="column" alignItems="center">
-                      <Grid item>
-                        <LightTooltip title="Tambahkan Berkas" placement="right">
-                          <IconButton>
-                            <AttachFileIcon />
-                          </IconButton>
-                        </LightTooltip>
-                      </Grid>
-                      <Grid item>
-                        <LightTooltip title="Duplikat Soal" placement="right">
-                          <IconButton>
-                            <FilterNoneIcon />
-                          </IconButton>
-                        </LightTooltip>
-                      </Grid>
-                      <Grid item>
-                        <LightTooltip title="Hapus Soal" placement="right">
-                          <IconButton>
-                            <DeleteIcon />
-                          </IconButton>
-                        </LightTooltip>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </Paper>
-            </Grid>
-            <Grid item>
-              <Paper>
-                <Grid container>
-                  <Grid item xs sm className={classes.paperContent}>
-                    <Grid container direction="column" spacing={2}>
-                      <Grid item>
-                        <Typography variant="h6" gutterBottom>
-                          Soal 1
-                        </Typography>
-                        <TextField multiline fullWidth variant="filled" />
-                      </Grid>
-                      <Grid item>
-                        <FormControl component="fieldset">
-                          <RadioGroup value={this.state.radioValue} onChange={this.handleRadioValue}>
-                            <FormControlLabel value="female" control={<Radio />} label="Female" />
-                            <FormControlLabel value="male" control={<Radio />} label="Male" />
-                            <FormControlLabel value="other" control={<Radio />} label="Other" />
-                            <FormControlLabel value="disabled" disabled control={<Radio />} label="(Disabled option)" />
-                            <FormControlLabel disabled control={<Radio />} label={<TextField />} />
-                          </RadioGroup>
-                        </FormControl>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                  <Divider flexItem orientation="vertical" />
-                  <Grid item xs={3} sm={1} className={classes.paperContent}>
-                    <Grid container direction="column" alignItems="center">
-                      <Grid item>
-                        <LightTooltip title="Tambahkan Berkas" placement="right">
-                          <IconButton>
-                            <AttachFileIcon />
-                          </IconButton>
-                        </LightTooltip>
-                      </Grid>
-                      <Grid item>
-                        <LightTooltip title="Duplikat Soal" placement="right">
-                          <IconButton>
-                            <FilterNoneIcon />
-                          </IconButton>
-                        </LightTooltip>
-                      </Grid>
-                      <Grid item>
-                        <LightTooltip title="Hapus Soal" placement="right">
-                          <IconButton>
-                            <DeleteIcon />
-                          </IconButton>
-                        </LightTooltip>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </Paper>
-            </Grid>
+function ViewAssessmentStudent() {
+  const classes = useStyles();
+  
+  return (
+    <div className={classes.root}>
+      <form>
+        <Grid container direction="column" spacing={3}>
+          <Grid item>
+            <Paper>
+            </Paper>
           </Grid>
-        </form>
-      </div>
-    )
-  }
+          <Grid item>
+            <Paper>
+              <Grid container direction="column" spacing={3} justify="center" className={classes.content}>
+                <Grid item>
+                  <Typography variant="h5" gutterBottom>
+                    Judul Kuis
+                  </Typography>
+                  <Typography>
+                    Deskripsi Kuis
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  {/*<CountdownCircleTimer
+                    isPlaying
+                    duration={10}
+                    colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
+                    onComplete={() => [true, 1000]}
+                  >
+                    {renderTime}
+                  </CountdownCircleTimer> */}
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+          <Grid item>
+            <Paper>
+              <Grid container>
+                <Grid item xs sm className={classes.content}>
+                  <Grid container direction="column" spacing={2}>
+                    <Grid item>
+                      <Typography variant="h6" gutterBottom>
+                        Soal 1
+                      </Typography>
+                      <TextField multiline fullWidth variant="filled" />
+                    </Grid>
+                    <Grid item>
+                      <FormControl component="fieldset">
+                        <RadioGroup value="" onChange="">
+                          <FormControlLabel value="female" control={<Radio />} label="Female" />
+                          <FormControlLabel value="male" control={<Radio />} label="Male" />
+                          <FormControlLabel value="other" control={<Radio />} label="Other" />
+                          <FormControlLabel value="disabled" disabled control={<Radio />} label="(Disabled option)" />
+                          <FormControlLabel disabled control={<Radio />} label={<TextField />} />
+                        </RadioGroup>
+                      </FormControl>
+                    </Grid>
+                  </Grid>
+                </Grid>
+                <Divider flexItem orientation="vertical" />
+                <Grid item xs={3} sm={1} className={classes.content}>
+                  <Grid container direction="column" alignItems="center">
+                    <Grid item>
+                      <LightTooltip title="Tambahkan Berkas" placement="right">
+                        <IconButton>
+                          <AttachFileIcon />
+                        </IconButton>
+                      </LightTooltip>
+                    </Grid>
+                    <Grid item>
+                      <LightTooltip title="Duplikat Soal" placement="right">
+                        <IconButton>
+                          <FilterNoneIcon />
+                        </IconButton>
+                      </LightTooltip>
+                    </Grid>
+                    <Grid item>
+                      <LightTooltip title="Hapus Soal" placement="right">
+                        <IconButton>
+                          <DeleteIcon />
+                        </IconButton>
+                      </LightTooltip>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+  )
 };
 
-export default (withStyles(styles)(ViewAssessmentStudent));
+export default ViewAssessmentStudent;
