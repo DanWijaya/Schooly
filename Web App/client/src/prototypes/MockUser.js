124cb47d4235f0ec505ae7dd9f89b5b6dbdaf89d
Gregorius Dimas Baskara <gregdimasbaskara@gmail.com>
2020-08-19 14:00:48 +0700

    Model

---
+++ Web App/client/src/prototypes/MockUser.js
@@ -0,0 +1,56 @@
+// Require Mongoose
+const mongoose = require("mongoose");
+// API dari mongoose MongoDb. 
+
+// Define a Schema
+const Schema = mongoose.Schema;
+
+const options = {discriminatorKey: "role"};
+
+// Create Schema (New way)
+
+const MockUserSchema = new Schema({
+  avatar: {
+    type: String,
+    default: ""
+  },
+  // Informasi Pribadi
+  name: {
+    type: String,
+    required: true
+  },
+  tanggal_lahir: {
+    type: Date,
+    default: new Date()
+  },
+  jenis_kelamin: {
+    type: String,
+    default: null
+  },
+  sekolah: {
+    type: String,
+    default: null
+  },
+
+  //Kontak
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  emergency_phone: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+}, options )
+
+const MockUser = mongoose.model("mock_users", MockUserSchema);
+
+module.exports = MockUser;
