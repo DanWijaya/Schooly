5e3118e6bf9232c397114a751c414aef69d5770f
Haydrent <handersen_isaac@yahoo.com>
2020-12-28 16:31:55 +0700

    Fixed footer yang kelebihan divider

--- Web App/client/src/components/misc/footer/Footer.js
+++ Web App/client/src/components/misc/footer/Footer.js
@@ -39,94 +39,99 @@ function Footer(props) {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Divider style={{marginBottom: "10px"}} />
       <Hidden mdUp implementation="css">
         {/* Mobile = Column View */}
         {(props.assessmentState !== "ujian") ?
-          <Grid container justify="space-between">
-            <Grid item>
-              <div className={classes.footerMobileContainer}>
-                <Typography variant="caption" color="textSecondary">
-                  Schooly System
-                </Typography>
-                <img src={schoolySymbolLogo} alt="Schooly Symbol Logo" className={classes.schoolySymbolMobile} />
-              </div>
+          <div>
+            <Divider style={{marginBottom: "10px"}} />
+            <Grid container justify="space-between">
+              <Grid item>
+                <div className={classes.footerMobileContainer}>
+                  <Typography variant="caption" color="textSecondary">
+                    Schooly System
+                  </Typography>
+                  <img src={schoolySymbolLogo} alt="Schooly Symbol Logo" className={classes.schoolySymbolMobile} />
+                </div>
+              </Grid>
+              <Grid item>
+                <div className={classes.footerMobileContainer}>
+                  <Typography variant="caption">
+                    <Link to="/bantuan">
+                      Bantuan
+                    </Link>
+                  </Typography>
+                  <Typography variant="caption">
+                    <Link to="/tentang-schooly">
+                      Tentang Schooly
+                    </Link>
+                  </Typography>
+                  <Typography variant="caption">
+                    <Link to="/kebijakan-penggunaan">
+                      Kebijakan Pengunaan
+                    </Link>
+                  </Typography>
+                </div>
+              </Grid>
+              <Grid item>
+                <div className={classes.footerMobileContainer}>
+                  <Typography variant="caption">
+                    <Link to="mailto:schoolysystem@gmail.com">
+                      Hubungi Kami
+                    </Link>
+                  </Typography>
+                  <Typography variant="caption">
+                    <Link to="http://www.instagram.com">
+                      Instagram
+                    </Link>
+                  </Typography>
+                </div>
+              </Grid>
             </Grid>
-            <Grid item>
-              <div className={classes.footerMobileContainer}>
-                <Typography variant="caption">
-                  <Link to="/bantuan">
-                    Bantuan
-                  </Link>
-                </Typography>
-                <Typography variant="caption">
-                  <Link to="/tentang-schooly">
-                    Tentang Schooly
-                  </Link>
-                </Typography>
-                <Typography variant="caption">
-                  <Link to="/kebijakan-penggunaan">
-                    Kebijakan Pengunaan
-                  </Link>
-                </Typography>
-              </div>
-            </Grid>
-            <Grid item>
-              <div className={classes.footerMobileContainer}>
-                <Typography variant="caption">
-                  <Link to="mailto:schoolysystem@gmail.com">
-                    Hubungi Kami
-                  </Link>
-                </Typography>
-                <Typography variant="caption">
-                  <Link to="http://www.instagram.com">
-                    Instagram
-                  </Link>
-                </Typography>
-              </div>
-            </Grid>
-          </Grid>
+          </div>
         :
           null
         }
       </Hidden>
       <Hidden smDown implementation="css">
         {/* Desktop = Row View */}
-        {(props.assessmentState !== "ujian") ? 
-        <div className={classes.footerDesktopContainer}>
-          <Grid item container spacing={2} justify="flex-start">
-            <Grid item style={{color: "grey"}}>
-              Schooly System
+        {(props.assessmentState !== "ujian") ?
+        <div>
+          <Divider style={{marginBottom: "10px"}} />
+          <div className={classes.footerDesktopContainer}>
+            <Grid item container spacing={2} justify="flex-start">
+              <Grid item style={{color: "grey"}}>
+                Schooly System
+              </Grid>
+              <Grid item>
+                <Link to="/bantuan">
+                  Bantuan
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link to="/kebijakan-penggunaan">
+                  Kebijakan Pengunaan
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link to="/bantuan">
-                Bantuan
-              </Link>
+            <img src={schoolySymbolLogo} alt="Schooly Symbol Logo" className={classes.schoolySymbolDesktop} />
+            <Grid container spacing={2} justify="flex-end">
+              <Grid item>
+                <Link to="/tentang-schooly">
+                  Tentang Schooly
+                </Link>
+              </Grid>
+              <Grid item>
+                <a href="mailto:schoolysystem@gmail.com">
+                  Hubungi Kami
+                </a>
+              </Grid>
+              <Grid item>
+                <a href="http://www.instagram.com">
+                  Instagram
+                </a>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link to="/kebijakan-penggunaan">
-                Kebijakan Pengunaan
-              </Link>
-            </Grid>
-          </Grid>
-          <img src={schoolySymbolLogo} alt="Schooly Symbol Logo" className={classes.schoolySymbolDesktop} />
-          <Grid container spacing={2} justify="flex-end">
-            <Grid item>
-              <Link to="/tentang-schooly">
-                Tentang Schooly
-              </Link>
-            </Grid>
-            <Grid item>
-              <a href="mailto:schoolysystem@gmail.com">
-                Hubungi Kami
-              </a>
-            </Grid>
-            <Grid item>
-              <a href="http://www.instagram.com">
-                Instagram
-              </a>
-            </Grid>
-          </Grid>
+          </div>
         </div>
         :
         null
