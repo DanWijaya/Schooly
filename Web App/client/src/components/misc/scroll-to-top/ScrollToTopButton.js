c71e25fea9508d82baea76b2b6315a9098c809e1
Haydrent <handersen_isaac@yahoo.com>
2021-08-20 12:49:47 +0700

    Fixed scroll to top button

--- Web App/client/src/components/misc/scroll-to-top/ScrollToTopButton.js
+++ Web App/client/src/components/misc/scroll-to-top/ScrollToTopButton.js
@@ -5,7 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  scrollToTop: {
     position: "fixed",
     bottom: "15px",
     right: "15px",
@@ -15,9 +15,7 @@ const useStyles = makeStyles((theme) => ({
 function ScrollToTopButton(props) {
   const { window } = props;
   const classes = useStyles();
-  // Note that you normally won"t need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
+
   const trigger = useScrollTrigger({
     target: window ? window() : undefined,
     disableHysteresis: true,
@@ -28,13 +26,13 @@ function ScrollToTopButton(props) {
     const anchor = (event.target.ownerDocument || document).querySelector("#back-to-top-anchor");
 
     if (anchor) {
-      anchor.scrollIntoView({ behavior: "smooth", block: "center" });
+      anchor.scrollIntoView({ behavior: "smooth", block: "start" });
     }
   };
 
   return (
     <Zoom in={trigger}>
-      <div onClick={handleClick} className={classes.root}>
+      <div onClick={handleClick} className={classes.scrollToTop}>
         <Fab color="primary" size="small">
           <KeyboardArrowUpIcon />
         </Fab>
