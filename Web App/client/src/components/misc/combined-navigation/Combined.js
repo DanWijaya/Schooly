872e5e3fbb832ec18a84ef8b7ca3d67455c87df6
DanWijaya <danwijaya19@gmail.com>
2021-08-25 21:06:00 +0800

    Make changes to NavBar and SideDrawer to prevent rerendering issue

---
+++ Web App/client/src/components/misc/combined-navigation/Combined.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+import NavBar from '../nav-bar/NavBar'
+import SideDrawer from '../side-drawer/SideDrawer'
+
+function Combined(props) {
+    const {showNavBar, sideDrawerExist } = props;
+    // const [showNavBar, setShowNavBar] = useState(true);
+    // const [sideDrawerExist, setSideDrawerExist] = useState(true);
+    const [mobileOpen, setMobileOpen] = useState(false);
+    const [desktopOpen, setDesktopOpen] = useState(false);
+
+    const handleDrawerDesktop = () => {
+        setDesktopOpen(!desktopOpen);
+    };
+
+    const handleDrawerMobile = () => {
+        setMobileOpen(!mobileOpen);
+    }
+
+    return (
+        <>
+        {showNavBar ? 
+         <NavBar
+          handleDrawerDesktop={handleDrawerDesktop}
+          handleDrawerMobile={handleDrawerMobile}
+          sideDrawerExist={sideDrawerExist}
+          assessmentState={localStorage.getItem(`status`)}
+        /> : null }
+
+        {sideDrawerExist && localStorage.getItem(`status`) !== "ujian" ? 
+         <SideDrawer
+          mobileOpen={mobileOpen}
+          desktopOpen={desktopOpen}
+          handleDrawerMobile={handleDrawerMobile}
+        /> : null}
+
+        </>
+    )
+}
+
+export default Combined;
