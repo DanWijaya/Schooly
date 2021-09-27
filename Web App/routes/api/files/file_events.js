b72019284ec869a7da582fc8cede892987b20f5f
DanWijaya <danwijaya19@gmail.com>
2021-09-27 23:24:16 +0800

    Fix Event Upload and Delete Function

--- Web App/routes/api/files/file_events.js
+++ Web App/routes/api/files/file_events.js
@@ -10,8 +10,8 @@ const { ObjectId } = require("mongodb");
 const { v4: uuidv4 } = require("uuid");
 
 // Multer ships with storage engines DiskStorage and MemoryStorage
-// And Multer adds a body object and a file or files object to the request object. 
-// The body object contains the values of the text fields of the form, 
+// And Multer adds a body object and a file or files object to the request object.
+// The body object contains the values of the text fields of the form,
 // the file or files object contains the files uploaded via the form.
 var storage = multer.memoryStorage();
 var upload = multer({ storage: storage });
@@ -24,58 +24,60 @@ AWS.config.update({
 
 // route to upload a pdf document file
 // In upload.single("file") - the name inside the single-quote is the name of the field that is going to be uploaded.
-router.post("/upload/:id", upload.array("lampiran_event"), (req, res) => {
-  const { files } = req;
-  let s3bucket = new AWS.S3({
-    accessKeyId: keys.awsKey.AWS_ACCESS_KEY_ID,
-    secretAccessKey: keys.awsKey.AWS_SECRET_ACCESS_KEY,
-    region: keys.awsKey.AWS_REGION,
-  });
-  // var ResponseData =[]
-  //Where you want to store your file
-
-  var numsFileUploaded = 0;
-  files.map((file) => {
-    var params = {
-      Bucket: keys.awsKey.AWS_BUCKET_NAME,
-      Key: "event/" + uuidv4() + "_" + file.originalname,
-      Body: file.buffer,
-      ContentType: file.mimetype,
-      ContentDisposition: `inline;filename=${file.originalname}`,
-    };
-    s3bucket
-      .upload(params, function (err, data) {
-        if (err) {
-          return res.status(500).json({ error: true, Message: err });
-        } else {
-          var newFileUploaded = {
-            filename: file.originalname,
-            s3_key: params.Key,
-            s3_directory: "event/",
-            event_id: req.params.id,
-          };
-          var document = new FileEvent(newFileUploaded);
-          document.save(function (error, newFile) {
-            if (error) {
-              throw error;
-            }
-            console.log(newFile);
-          });
-        }
-      })
-      .on("httpUploadProgress", function (data) {
-        if (data.loaded == data.total) {
-          numsFileUploaded++;
-          if (numsFileUploaded == files.length) {
-            return res.json({
-              _id: req.params.id,
-              success: "Successfully uploaded the lampiran file",
-            });
-          }
-        }
+router.post(
+  "/upload/:event_id",
+  upload.array("lampiran_event"),
+  async (req, res) => {
+    try {
+      const { files } = req;
+      const { event_id } = req.params;
+      let s3bucket = new AWS.S3({
+        accessKeyId: keys.awsKey.AWS_ACCESS_KEY_ID,
+        secretAccessKey: keys.awsKey.AWS_SECRET_ACCESS_KEY,
+        region: keys.awsKey.AWS_REGION,
       });
-  });
-});
+
+      const promises = files.map((file) => {
+        var params = {
+          Bucket: keys.awsKey.AWS_BUCKET_NAME,
+          Key: "event/" + uuidv4() + "_" + file.originalname,
+          Body: file.buffer,
+          ContentType: file.mimetype,
+          ContentDisposition: `inline;filename=${file.originalname}`,
+        };
+
+        return new Promise((resolve, reject) => {
+          s3bucket
+            .upload(params, function (err, data) {
+              if (err) {
+                reject({ error: true, Message: err });
+              }
+            })
+            .on("httpUploadProgress", function (data) {
+              if (data.loaded == data.total) {
+                let newFileUploaded = {
+                  filename: file.originalname,
+                  s3_key: params.Key,
+                  s3_directory: "event/",
+                  event_id: event_id,
+                };
+                let document = new FileEvent(newFileUploaded);
+                resolve(document);
+              }
+            });
+        });
+      });
+      const documents = await Promise.all(promises);
+      await FileEvent.insertMany(documents);
+      return res.json({
+        _id: event_id,
+        success: "Successfully uploaded the file",
+      });
+    } catch (err) {
+      return res.status(500).json({ error: true, Message: err });
+    }
+  }
+);
 
 router.get("/download/:id", (req, res) => {
   let s3bucket = new AWS.S3();
@@ -94,68 +96,80 @@ router.get("/download/:id", (req, res) => {
   });
 });
 
-// Router to delete a DOCUMENT file
-router.delete("/:id", (req, res) => {
-  const { file_to_delete } = req.body;
-  // if file_to_delete is undefined,means that the object is deleted and hence all files should be deleted.
-  if (!file_to_delete) {
-    FileEvent.find({ event_id: req.params.id }).then((events) => {
-      let id_list = events.map((m) => ObjectId(m._id));
-      let file_to_delete = events;
-
-      FileEvent.deleteMany(
-        {
-          _id: {
-            $in: id_list,
-          },
-        },
-        function (err, results) {
-          if (!results) {
-            return res.status(404).json(err);
-          }
-          //Now Delete the file from AWS-S3
-          // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#deleteObject-property
-          let s3bucket = new AWS.S3();
-          file_to_delete.forEach((file) => {
-            let params = {
-              Bucket: keys.awsKey.AWS_BUCKET_NAME,
-              Key: file.s3_key,
-            };
-            s3bucket.deleteObject(params, (err, data) => {
-              if (!data) return res.status(404).json(err);
-            });
-          });
-          return res.status(200).send("Success");
-        }
-      );
+router.delete("/all/:id", async (req, res) => {
+  try {
+    // req.params.id ini berupa id dari event.
+    const file_to_delete = await FileEvent.find({ event_id: req.params.id });
+    // file_to_delete ini berupa ID dari file filenya.
+    if (!file_to_delete) {
+      return res.status(200).json("No file event to delete");
+    }
+    await FileEvent.deleteMany({
+      event_id: req.params.id,
     });
-  } else {
-    let id_list = file_to_delete.map((m) => ObjectId(m._id));
-    FileEvent.deleteMany(
-      {
-        _id: {
-          $in: id_list,
-        },
-      },
-      function (err, results) {
-        if (!results) {
-          return res.status(404).json(err);
-        }
 
-        let s3bucket = new AWS.S3();
-        file_to_delete.forEach((file) => {
-          let params = {
-            Bucket: keys.awsKey.AWS_BUCKET_NAME,
-            Key: file.s3_key,
-          };
-          s3bucket.deleteObject(params, (err, data) => {
-            console.log("Data event: ", data);
-            if (err) return res.status(404).json(err);
+    const promises = file_to_delete.map((file) => {
+      let s3bucket = new AWS.S3();
+      let params = {
+        Bucket: keys.awsKey.AWS_BUCKET_NAME,
+        Key: file.s3_key,
+      };
+
+      return new Promise((resolve, reject) => {
+        s3bucket
+          .deleteObject(params, (err, data) => {
+            if (!data) {
+              reject(err);
+            }
+          })
+          .on("httpDone", function (data) {
+            resolve();
           });
-        });
-        return res.status(200).send("Success");
-      }
-    );
+      });
+    });
+
+    await Promise.all(promises);
+    return res.status(200).json("Success");
+  } catch (err) {
+    return res.status(404).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { file_to_delete } = req.body;
+  // file_to_delete ini berupa ID dari file filenya.
+  if (!file_to_delete) {
+    return res.status(200).send("No file events to delete");
+  }
+  try {
+    // if file_to_delete is undefined,means that the object is deleted and hence all files should be deleted.
+    let id_list = file_to_delete.map((m) => ObjectId(m._id));
+    const results = await FileEvent.find({ _id: { $in: id_list } });
+    await FileEvent.deleteMany({ _id: { $in: id_list } });
+
+    const promises = results.map((file) => {
+      let s3bucket = new AWS.S3();
+      let params = {
+        Bucket: keys.awsKey.AWS_BUCKET_NAME,
+        Key: file.s3_key,
+      };
+      return new Promise((resolve, reject) => {
+        s3bucket
+          .deleteObject(params, (err, data) => {
+            if (!data) {
+              reject(err);
+            }
+          })
+          .on("httpDone", function (data) {
+            resolve();
+          });
+      });
+    });
+
+    await Promise.all(promises);
+    return res.status(200).send("Success");
+  } catch (err) {
+    return res.status(404).json(err);
   }
 });
 
