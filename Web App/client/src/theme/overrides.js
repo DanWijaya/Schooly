3bb6b93cb9d2d49a18f1de367284ea783dc96f67
DanWijaya <danwijaya19@gmail.com>
2021-12-27 23:49:33 +0700

    Rough code for AWS SES API setup

--- Web App/client/src/theme/overrides.js
+++ Web App/client/src/theme/overrides.js
@@ -5,12 +5,7 @@ import TextFields from "./overrides/TextFields";
 import Tooltip from "./overrides/Tooltip";
 
 function overrides(theme) {
-  return merge(
-    Lists(theme),
-    Pickers(theme),
-    TextFields(theme),
-    Tooltip(theme),
-  );
+  return merge(Lists(theme), Pickers(theme), TextFields(theme), Tooltip(theme));
 }
 
 export default overrides;
